9f910e512b7e199b40836ff4af2c61e088ffdb77
Christopher R. Genovese <genovese@cmu.edu>
2023-09-20 11:03:14 -0400

    fix: added numeric_sqrt to environment; omitted earlier by mistake

--- src/frplib/repls/playground_repl.py
+++ src/frplib/repls/playground_repl.py
@@ -107,7 +107,7 @@ playground_imports: dict[str, list[str]] = {
     ],
     'calculate': ['substitute', 'substitute_with', 'substitution'],
     'numeric': ['numeric_exp', 'numeric_ln', 'numeric_log10', 'numeric_log2',
-                'numeric_abs', 'numeric_floor', 'numeric_ceil'],
+                'numeric_abs', 'numeric_sqrt', 'numeric_floor', 'numeric_ceil'],
     'quantity': ['as_quantity', 'qvec'],
     'symbolic': ['gen_symbol', 'is_symbolic', 'symbol'],
     'utils': [
